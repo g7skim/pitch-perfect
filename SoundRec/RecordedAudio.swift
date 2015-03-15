5cf1bd14905bd35825840d2561b3f4caba2ca1d4
g7skim <jamuday@me.com>
2015-03-15 09:43:10 +0300

    Committed by Udacity

---
+++ SoundRec/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  SoundRec
+//
+//  Created by g7skim on 14.03.15.
+//  Copyright (c) 2015 Jamuday. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
